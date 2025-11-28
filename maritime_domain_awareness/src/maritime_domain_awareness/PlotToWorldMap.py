4fc3c5d448bb7abf2873b813189361c9a3118fd9
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2025-11-28 23:44:41 +0100

    push

--- maritime_domain_awareness/src/maritime_domain_awareness/PlotToWorldMap.py
+++ maritime_domain_awareness/src/maritime_domain_awareness/PlotToWorldMap.py
@@ -1,33 +1,27 @@
 import cartopy.crs as ccrs
 from cartopy.feature import NaturalEarthFeature
 import matplotlib.pyplot as plt
+import numpy as np
 
-def PlotToWorldMap(actualPoint=None, predictedPoint=None):
+def PlotToWorldMap(train_val_data=None, test_actual=None, test_predicted=None):
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.stock_img()
     ax.add_feature(NaturalEarthFeature('physical', 'land', '50m', edgecolor='face', facecolor='lightgreen'))
     ax.add_feature(NaturalEarthFeature('physical', 'ocean', '50m', edgecolor='face', facecolor='lightblue'))
 
-    # Flatten and convert tensors to lists of coordinates
-    actual_lons = []
-    actual_lats = []
-    predicted_lons = []
-    predicted_lats = []
+   
+    def extract_coords(data):
+        data_np = data.detach().cpu().numpy()
+        return data_np[:, 0], data_np[:, 1] 
 
-    for act, pred in zip(actualPoint, predictedPoint):
-        act_np = act.detach().cpu().numpy()  # shape: (batch, seq_len, 2) or (seq_len, 2)
-        pred_np = pred.detach().cpu().numpy()
-        # Flatten all points
-        act_np = act_np.reshape(-1, 2)
-        pred_np = pred_np.reshape(-1, 2)
-        for pt in act_np:
-            actual_lons.append(pt[1])
-            actual_lats.append(pt[0])
-        for pt in pred_np:
-            predicted_lons.append(pt[1])
-            predicted_lats.append(pt[0])
-            
-    ax.plot(actual_lons, actual_lats, 'o-', color='green', label='Actual', transform=ccrs.PlateCarree())
-    ax.plot(predicted_lons, predicted_lats, 'x-', color='red', label='Predicted', transform=ccrs.PlateCarree())
+    lats, lons = extract_coords(train_val_data)
+    ax.plot(lons, lats, '-', color='blue', label='Train+Val', transform=ccrs.PlateCarree())
+
+    lats, lons = extract_coords(test_actual)
+    ax.plot(lons, lats, 'o-', color='green', label='Actual', transform=ccrs.PlateCarree())
+
+    lats, lons = extract_coords(test_predicted)
+    ax.plot(lons, lats, 'x-', color='red', label='Predicted', transform=ccrs.PlateCarree())
+
     ax.legend()
     plt.show()
